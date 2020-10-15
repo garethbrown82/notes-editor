66d3e033083dd4879f83023dfd82fc151e7e9f4c
Gareth Brown <gareth.brown@bjss.com>
2020-10-15 07:41:47 +0100

    Put get request logic in try catch block

--- functions/notes.js
+++ functions/notes.js
@@ -11,20 +11,26 @@ admin.initializeApp();
 app.get('/', async (request, response) => {
   functions.logger.info('Triggering get notes request', { structuredData: true });
 
-  // Use admin to access a reference to the notes collection in Firestore
-  const notesRef = admin.firestore().collection('notes');
-
-  // Get all notes from the notes collection
-  const notesCollection = await notesRef.get();
-
-  // Add notes document data to an array
-  let notes = [];
-  notesCollection.forEach((note) => {
-    notes.push(note.data());
-  });
-
-  // return notes array as a json response
-  response.json(notes);
+  try {
+    // Use admin to access a reference to the notes collection in Firestore
+    const notesRef = admin.firestore().collection('notes');
+  
+    // Get all notes from the notes collection
+    const notesCollection = await notesRef.get();
+  
+    // Add notes document data to an array
+    let notes = [];
+    notesCollection.forEach((note) => {
+      notes.push(note.data());
+    });
+  
+    // return notes array as a json response
+    response.json(notes);
+  } catch (error) {
+    // Log and return error status if things go wrong
+    functions.logger.error('There was an error while retrieving notes from Firestore.', error);
+    response.status(500).send('Internal server error');
+  }
 });
 
 exports.notes = functions.https.onRequest(app);
