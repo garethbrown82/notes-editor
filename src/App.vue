54d22333ad3ff999a9a56880c9eca6faa997e1f0
Gareth Brown <gareth.brown@bjss.com>
2020-11-02 09:00:08 +0000

    Display notes using component data

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <NotesList msg="Welcome to Your Vue.js App" />
+  <div class="wrapper">
+    <NotesList />
+  </div>
 </template>
 
 <script>
@@ -18,8 +20,14 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.wrapper {
+  width: 40%;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
 }
 </style>
--- src/components/NotesList.vue
+++ src/components/NotesList.vue
@@ -1,10 +1,24 @@
 <template>
-  <p>Notes go here...</p>
+  <h1>Notes</h1>
+  <!-- Render the array of notes using v-for directive just like a for loop -->
+  <div v-for="note in notes" v-bind:key="note.title">
+    <h3>{{ note.title }}</h3>
+    <p>{{ note.text }}</p>
+  </div>
 </template>
 
 <script>
 export default {
-  
+  data() {
+    return {
+      // Store the notes using the components local data (component state)
+      notes: [
+        { title: 'Shopping List', text: 'Tomatoes, Milk, Salt' },
+        { title: 'Book', text: 'Name of the wind' },
+        { title: 'Note', text: 'This is a note' },
+      ],
+    }
+  }
 }
 </script>
 
